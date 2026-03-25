45103c9d0063d5cca3dfe543e339d95961c7e2aa
gagankarthik <gagankarthik123@gmail.com>
2026-03-25 14:26:23 -0400

    Removed Sidebar

--- src/components/dashboard/index.ts
+++ src/components/dashboard/index.ts
@@ -1,4 +1,3 @@
-export { default as Sidebar } from './Sidebar';
 export { default as StatsCards } from './StatsCards';
 export { default as FilterBar } from './FilterBar';
 export { default as EmployeeTable } from './EmployeeTable';
